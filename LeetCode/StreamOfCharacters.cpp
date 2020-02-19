8657638cc67a61432cb9b7314a400d9bf0ced5e9
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-19 12:53:31 +0530

    IMP revised

--- LeetCode/StreamOfCharacters.cpp
+++ LeetCode/StreamOfCharacters.cpp
@@ -1,61 +1,54 @@
 class Trie {
-    unordered_map<char, Trie*> char_map;
-    bool word_ends;
-    public:
-        Trie() {
-            word_ends = false;
-        }
-        
-        void insert(string word) {
-            if(!word.size())
-                return;
-            
-            Trie *temp = this;
-            for(auto ch : word) {
-                if(temp -> char_map.find(ch) != temp -> char_map.end())
-                    temp = temp -> char_map[ch];
-                else {
-                    temp -> char_map[ch] = new Trie();
-                    temp = temp -> char_map[ch];
-                }
-            }
-            temp -> word_ends = true;
-        }
+    unordered_map<char, Trie*> next;
+    bool wordEnds;
     
-        bool search(string word) {
-            if(!word.size())
-                return false;
-            Trie *temp = this;
-            for(auto ch : word) {
-                if(temp -> char_map.find(ch) == temp -> char_map.end()) {
-                    return false;
-                }
-                temp = temp -> char_map[ch];
-                if(temp -> word_ends)
-                    return true;
+    public:
+    Trie() {
+        wordEnds = false;
+    }
+    void insert(string word) {
+        Trie* temp = this;
+        for(char ch : word) {
+            if(temp -> next.find(ch) == temp -> next.end()) {
+                temp -> next[ch] = new Trie();
             }
-            return temp -> word_ends;
+            temp = temp -> next[ch];
         }
+        temp -> wordEnds = true;
+    }
+    
+    bool search(string word) {
+        Trie* temp = this;
+        for(char ch : word) {
+            if(temp -> next.find(ch) == temp -> next.end()) {
+                return false;
+            }
+            temp = temp -> next[ch];
+            if(temp -> wordEnds)
+                return true;
+        }
+        return temp -> wordEnds;
+    }
 };
 
 class StreamChecker {
 public:
-    Trie my_trie;
-    string str = "";
-    int wLen = 0;
+    Trie trieNode;
+    int maxLen = 0;
+    string currStr;
     StreamChecker(vector<string>& words) {
-        for(auto w : words) {
-            reverse(w.begin(), w.end());
-            wLen = max(wLen, (int)w.length());
-            my_trie.insert(w);
+        for(string word : words) {
+            reverse(word.begin(), word.end());
+            trieNode.insert(word);
+            maxLen = max(maxLen, (int)word.size());
         }
     }
     
     bool query(char letter) {
-        str = letter + str;
-        if(str.length() > wLen)
-            str.pop_back();
-        return my_trie.search(str);
+        if(currStr.size() > maxLen)
+            currStr.pop_back();
+        currStr = letter + currStr;
+        return trieNode.search(currStr);
     }
 };
 
